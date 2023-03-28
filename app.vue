f6702a00b2044963d4aff39e7595fc9cb99e5598
姚届云 <yjy18593128603@163.com>
2023-03-28 10:37:52 +0800

    新增layout布局

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
+  <NuxtLink to="/index"/>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <NuxtLayout :name="layout">
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+let layout: string = "layout"
+
+</script>
--- layouts/layout.vue
+++ layouts/layout.vue
@@ -1,4 +1,11 @@
 <template>
-
-        
+    <LayoutHeader />
+    <div class="app-content">
+        <LayoutContentLeft />
+        <div class="projects-section">
+            <slot />
+        </div>
+        <LayoutContentRigth />
+    </div>
+    <LayoutFooter/>
 </template>
