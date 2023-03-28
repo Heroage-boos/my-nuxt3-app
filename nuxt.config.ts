accf3a52fbdfe18ffcede4db648b67ae4db1da5e
姚届云 <yjy18593128603@163.com>
2023-03-28 08:55:03 +0800

    添加sass；配置nuxt全局css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,12 @@ export default defineNuxtConfig({
       },
     },
   },
+  css: [
+    //定义要全局设置的 CSS 文件/模块/库（包含在每个页面中）。
+    // "bulma",//直接加载一个Node.js模块（这里是一个Sass文件）。
+    // "@/assets/css/main.css",//项目中的css文件
+    "@/assets/style/index.scss", //项目中的scss文件
+    "@/assets/style/common.scss",
+    "@/assets/style/layout.scss",
+  ],
 });
