82384bc928e1e0a0bdbbfea51881338fcf6c0d9f
姚届云 <yjy18593128603@163.com>
2023-03-29 15:53:57 +0800

    替换layoutheader中switch换肤色--待添加功能

--- components/layoutHeader.vue
+++ components/layoutHeader.vue
@@ -15,17 +15,11 @@
             </div>
         </div>
         <div class="app-header-right">
-            <button class="mode-switch" title="Switch Theme">
-                <svg class="moon" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="2" width="24" height="24" viewBox="0 0 24 24">
-                    <defs></defs>
-                    <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
-                </svg>
-            </button>
+            <themeSwitch />
             <button class="add-btn" title="Add New Project">
-                <svg class="btn-icon feather feather-plus" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-                    fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" 
-                   >
+                <svg class="btn-icon feather feather-plus" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round"
+                    stroke-linejoin="round">
                     <line x1="12" y1="5" x2="12" y2="19" />
                     <line x1="5" y1="12" x2="19" y2="12" />
                 </svg>
@@ -55,4 +49,9 @@
 </template>
 
 <script setup lang="ts">
+
+function handleTabBackground() {
+    const nuxt = document.body;
+    nuxt.style.backgroundColor = "darkgray";
+}
 </script>
